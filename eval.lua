bc48ae33e3c8f8a69a0ff9b9bebb77dd1dc13208
Genesis2305 <gurjeetsinghchatyal@gmail.com>
2017-11-04 19:18:47 +0530

    Eval commit

--- eval.lua
+++ eval.lua
@@ -24,3 +24,40 @@ cmd:option('-dump_path', 0, 'Write image paths along with predictions into vis j
 cmd:option('-sample_max', 1, '1 = sample argmax words. 0 = sample from distributions.')
 cmd:option('-beam_size', 2, 'used when sample_max = 1, indicates number of beams in beam search. Usually 2 or 3 works well. More is not better. Set this to 1 for faster runtime but a bit worse performance.')
 cmd:option('-temperature', 1.0, 'temperature when sampling from distributions (i.e. when sample_max = 0). Lower = "safer" predictions.')
+
+cmd:option('-image_folder', '', 'If this is nonempty then will predict on the images in this folder path')
+cmd:option('-image_root', '', 'In case the image paths have to be preprended with a root path to an image folder')
+
+cmd:option('-input_h5','','path to the h5file containing the preprocessed dataset. empty = fetch from model checkpoint.')
+cmd:option('-input_json','','path to the json file containing additional info and vocab. empty = fetch from model checkpoint.')
+cmd:option('-split', 'test', 'if running on MSCOCO images, which split to use: val|test|train')
+cmd:option('-coco_json', '', 'if nonempty then use this file in DataLoaderRaw (see docs there). Used only in MSCOCO test evaluation, where we have a specific json file of only test set images.')
+cmd:option('-backend', 'cudnn', 'nn|cudnn')
+cmd:option('-id', 'evalscript', 'an id identifying this run/job. used only if language_eval = 1 for appending to intermediate files')
+cmd:option('-seed', 123, 'random number generator seed to use')
+cmd:option('-gpuid', 0, 'which gpu to use. -1 = use CPU')
+cmd:text()
+
+
+local opt = cmd:parse(arg)
+torch.manualSeed(opt.seed)
+torch.setdefaulttensortype('torch.FloatTensor')
+if opt.gpuid >= 0 then
+  require 'cutorch'
+  require 'cunn'
+  if opt.backend == 'cudnn' then require 'cudnn' end
+  cutorch.manualSeed(opt.seed)
+  cutorch.setDevice(opt.gpuid + 1) 
+end
+assert(string.len(opt.model) > 0, 'must provide a model')
+local checkpoint = torch.load(opt.model)
+if string.len(opt.input_h5) == 0 then opt.input_h5 = checkpoint.opt.input_h5 end
+if string.len(opt.input_json) == 0 then opt.input_json = checkpoint.opt.input_json end
+if opt.batch_size == 0 then opt.batch_size = checkpoint.opt.batch_size end
+local fetch = {'rnn_size', 'input_encoding_size', 'drop_prob_lm', 'cnn_proto', 'cnn_model', 'seq_per_img'}
+for k,v in pairs(fetch) do 
+  opt[v] = checkpoint.opt[v] 
+end
+local vocab = checkpoint.vocab 
+
+
