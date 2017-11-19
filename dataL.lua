8f8ed8177a55174c8d8e833cac7c18d6c1b9e9cf
Genesis2305 <gurjeetsinghchatyal@gmail.com>
2017-11-19 17:35:47 +0530

    Done

--- dataL.lua
+++ dataL.lua
@@ -5,44 +5,44 @@ local DataLoader = torch.class('DataLoader')
 
 function DataLoader:__init(opt)
 
-print('DataLoader loading json file: ', opt.json_file)
+prnt('DataLoader loading json file: ', opt.json_file)
   self.info = utils.read_json(opt.json_file)
-  self.ix_to_word = self.info.ix_to_word
-  self.vocab_size = utils.count_keys(self.ix_to_word)
-  print('vocab size is ' .. self.vocab_size)
+  self.indx_wrd = self.info.indx_wrd
+  self.vocab_size = utils.count_keys(self.indx_wrd)
+  prnt('vocab size is ' .. self.vocab_size)
 
-print('DataLoader loading h5 file: ', opt.h5_file)
+prnt('DataLoader loading h5 file: ', opt.h5_file)
   self.h5_file = hdf5.open(opt.h5_file, 'r')
 
- local images_size = self.h5_file:read('/images'):dataspaceSize()
-  assert(#images_size == 4, '/images should be a 4D tensor')
-  assert(images_size[3] == images_size[4], 'width and height must match')
-  self.num_images = images_size[1]
-  self.num_channels = images_size[2]
-  self.max_image_size = images_size[3]
-  print(string.format('read %d images of size %dx%dx%d', self.num_images, 
-            self.num_channels, self.max_image_size, self.max_image_size))
+ local img_s = self.h5_file:read('/images'):dataspaceSize()
+  assert(#img_s == 4, '/images should be a 4D tensor')
+  assert(img_s[3] == img_s[4], 'width and height must match')
+  self.no_img = img_s[1]
+  self.no_chnls = img_s[2]
+  self.img_smax = img_s[3]
+  prnt(strng.format('read %d images of size %dx%dx%d', self.no_img, 
+            self.no_chnls, self.img_smax, self.img_smax))
 
- local seq_size = self.h5_file:read('/labels'):dataspaceSize()
-  self.seq_length = seq_size[2]
-  print('max sequence length in data is ' .. self.seq_length)
+ local s_seq = self.h5_file:read('/labels'):dataspaceSize()
+  self.len_seq = s_seq[2]
+  prnt('max sequence length in data is ' .. self.len_seq)
 
- self.label_start_ix = self.h5_file:read('/label_start_ix'):all()
-  self.label_end_ix = self.h5_file:read('/label_end_ix'):all()
+ self.start_indx = self.h5_file:read('/start_indx'):all()
+  self.end_indx = self.h5_file:read('/end_indx'):all()
 
- self.split_ix = {}
+ self.splt_indx = {}
   self.iterators = {}
   for i,img in pairs(self.info.images) do
     local split = img.split
-    if not self.split_ix[split] then
+    if not self.splt_indx[split] then
 
- self.split_ix[split] = {}
+ self.splt_indx[split] = {}
       self.iterators[split] = 1
     end
-    table.insert(self.split_ix[split], i)
+    table.insert(self.splt_indx[split], i)
   end
-  for k,v in pairs(self.split_ix) do
-    print(string.format('assigned %d images to split %s', #v, k))
+  for k,v in pairs(self.splt_indx) do
+    prnt(strng.format('assigned %d images to split %s', #v, k))
   end
 end
 
@@ -55,66 +55,66 @@ function DataLoader:getVocabSize()
 end
 
 function DataLoader:getVocab()
-  return self.ix_to_word
+  return self.indx_wrd
 end
 
 function DataLoader:getSeqLength()
-  return self.seq_length
+  return self.len_seq
 end
 
       function DataLoader:getBatch(opt)
   local split = utils.getopt(opt, 'split')
-local batch_size = utils.getopt(opt, 'batch_size', 5)
-local seq_per_img = utils.getopt(opt, 'seq_per_img', 5)
+local s_batch = utils.getopt(opt, 's_batch', 5)
+local seq_simg = utils.getopt(opt, 'seq_simg', 5)
 
-  local split_ix = self.split_ix[split]
-  assert(split_ix, 'split ' .. split .. ' not found.')
- local img_batch_raw = torch.ByteTensor(batch_size, 3, 256, 256)
-  local label_batch = torch.LongTensor(batch_size * seq_per_img, self.seq_length)
-  local max_index = #split_ix
-  local wrapped = false
+  local splt_indx = self.splt_indx[split]
+  assert(splt_indx, 'split ' .. split .. ' not found.')
+ local raw_imgb = torch.ByteTensor(s_batch, 3, 256, 256)
+  local batch = torch.LongTensor(s_batch * seq_simg, self.len_seq)
+  local indx_mx = #splt_indx
+  local wrap = false
   local infos = {}
-  for i=1,batch_size do
+  for i=1,s_batch do
 
 
-    local ri = self.iterators[split]
+    local r = self.iterators[split]
 
- local ri_next = ri + 1
-if ri_next > max_index then ri_next = 1; wrapped = true end
-self.iterators[split] = ri_next
-    ix = split_ix[ri]
-    assert(ix ~= nil, 'bug: split ' .. split .. ' was accessed out of bounds with ' .. ri)
-local img = self.h5_file:read('/images'):partial({ix,ix},{1,self.num_channels},
-                            {1,self.max_image_size},{1,self.max_image_size})
-    img_batch_raw[i] = img
-local ix1 = self.label_start_ix[ix]
-    local ix2 = self.label_end_ix[ix]
-    local ncap = ix2 - ix1 + 1
-assert(ncap > 0, 'an image does not have any label. this can be handled but right now isn\'t')
+ local nxtr = r + 1
+if nxtr > indx_mx then nxtr = 1; wrap = true end
+self.iterators[split] = nxtr
+    indx = splt_indx[r]
+    assert(indx ~= nil, 'bug: split ' .. split .. ' was accessed out of bounds with ' .. r)
+local img = self.h5_file:read('/images'):partial({indx,indx},{1,self.no_chnls},
+                            {1,self.img_smax},{1,self.img_smax})
+    raw_imgb[i] = img
+local indx1 = self.start_indx[indx]
+    local indx2 = self.end_indx[indx]
+    local cpn = indx2 - indx1 + 1
+assert(cpn > 0, 'an image does not have any label. this can be handled but rght now isn\'t')
     local seq
-    if ncap < seq_per_img then
- seq = torch.LongTensor(seq_per_img, self.seq_length)
-      for q=1, seq_per_img do
-        local ixl = torch.random(ix1,ix2)
-        seq[{ {q,q} }] = self.h5_file:read('/labels'):partial({ixl, ixl}, {1,self.seq_length})
+    if cpn < seq_simg then
+ seq = torch.LongTensor(seq_simg, self.len_seq)
+      for q=1, seq_simg do
+        local indxl = torch.random(indx1,indx2)
+        seq[{ {q,q} }] = self.h5_file:read('/labels'):partial({indxl, indxl}, {1,self.len_seq})
       end
     else
-local ixl = torch.random(ix1, ix2 - seq_per_img + 1)
+local indxl = torch.random(indx1, indx2 - seq_simg + 1)
 
- seq = self.h5_file:read('/labels'):partial({ixl, ixl+seq_per_img-1}, {1,self.seq_length})
+ seq = self.h5_file:read('/labels'):partial({indxl, indxl+seq_simg-1}, {1,self.len_seq})
     end
-    local il = (i-1)*seq_per_img+1
-    label_batch[{ {il,il+seq_per_img-1} }] = seq
-local info_struct = {}
-    info_struct.id = self.info.images[ix].id
-    info_struct.file_path = self.info.images[ix].file_path
-    table.insert(infos, info_struct)
+    local ls = (i-1)*seq_simg+1
+    batch[{ {ls,ls+seq_simg-1} }] = seq
+local st_inf = {}
+    st_inf.id = self.info.images[indx].id
+    st_inf.file_path = self.info.images[indx].file_path
+    table.insert(infos, st_inf)
   end
 
   local data = {}
-  data.images = img_batch_raw
-  data.labels = label_batch:transpose(1,2):contiguous()
-data.bounds = {it_pos_now = self.iterators[split], it_max = #split_ix, wrapped = wrapped}
+  data.images = raw_imgb
+  data.labels = batch:transpose(1,2):contiguous()
+data.bounds = {it_pos_now = self.iterators[split], it_max = #splt_indx, wrap = wrap}
   data.infos = infos
   return data
 end
